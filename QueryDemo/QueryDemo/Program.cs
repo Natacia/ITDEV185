4d50f1913a9896de01c3dd7974fa2753cf1e1bfe
Natacia <taylorn7@gmatc.matc.edu>
2020-03-18 20:59:45 -0500

    Assignment 7

---
+++ QueryDemo/QueryDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace QueryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Words word = new Words();
+            WriteLine("Here is a list of words. This program will identify the different parts of speech.");
+            WriteLine(word.SelectList());
+            Thread.Sleep(1000);
+
+            WriteLine("\nThe nouns are: ");
+            WriteLine(word.QueryNoun());
+
+            Thread.Sleep(1000);
+            WriteLine("\nThe verbs are: ");
+            WriteLine(word.QueryVerb());
+
+            Thread.Sleep(1000);
+            WriteLine("\nThe adjectives are: ");
+            WriteLine(word.QueryAdjective());
+
+            WriteLine("\nPress any key to exit...");
+            ReadKey();
+        }
+    }
+}
